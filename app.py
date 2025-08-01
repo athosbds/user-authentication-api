ebc42a8af24cefaaf46235af2ffa307858f355cd
Athos <69597077+athosbds@users.noreply.github.com>
2025-08-01 16:13:50 -0300

    logout and css

--- app.py
+++ app.py
@@ -48,5 +48,9 @@ def dashboard():
         return render_template('dashboard.html', name=name)
     else:
         return redirect(url_for('login'))
+@app.route('/logout')
+def logout():
+    session.clear()
+    return redirect(url_for('login'))
 if __name__ == '__main__':
     app.run(debug=True)
